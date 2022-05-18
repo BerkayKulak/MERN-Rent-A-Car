9eff7acf6f41a15aecf9b5b7eec43a3dac68f768
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-18 15:56:50 +0300

    Add new car - design

--- client/src/redux/actions/carsActions.js
+++ client/src/redux/actions/carsActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { message } from "antd";
 
 export const getAllCars = () => async (dispatch) => {
   dispatch({ type: "LOADING", payload: true });
@@ -10,6 +11,23 @@ export const getAllCars = () => async (dispatch) => {
     dispatch({ type: "GET_ALL_CARS", payload: response.data });
     dispatch({ type: "LOADING", payload: false });
   } catch (error) {
+    dispatch({ type: "LOADING", payload: false });
+  }
+};
+
+export const addCar = (reqObj) => async (dispatch) => {
+  dispatch({ type: "LOADING", payload: true });
+
+  try {
+    await axios.post("http://localhost:5000/api/cars/addcar", reqObj);
+
+    dispatch({ type: "LOADING", payload: false });
+    message.success("New car added successfully");
+    setTimeout(() => {
+      window.location.href = "/admin";
+    }, 500);
+  } catch (error) {
+    console.log(error);
     dispatch({ type: "LOADING", payload: false });
   }
 };
