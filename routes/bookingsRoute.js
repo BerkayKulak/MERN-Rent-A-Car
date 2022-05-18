995934d27abbe36a8e1cbce750d3727ab9b6684a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-18 15:09:26 +0300

    Payment gateway - backend

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,18 +2,47 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../models/bookingModel");
 const Car = require("../models/carModel");
+const { v4: uuidv4 } = require("uuid");
+const stripe = require("stripe")(
+  "sk_test_51IuZTULjHP9VtxkSjIkrD7E0bdk0F3bsQIecq6KpwkhUtwPFlpd4xx4RqCdDrD8dMbptDJbWyZjPSMEGRbC67S1y00W6ZbmMMz"
+);
 
 router.post("/bookcar", async (req, res) => {
   req.body.transactionId = "1234";
+  const { token } = req.body;
   try {
-    const newBooking = new Booking(req.body);
-    await newBooking.save();
-    const car = await Car.findOne({ _id: req.body.car });
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
 
-    car.bookedTimeSlots.push(req.body.bookedTimeSlots);
-    await car.save();
-    res.send("Booking Successful");
+    const payment = await stripe.charges.create(
+      {
+        amount: req.body.totalAmount * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
+
+    if (payment) {
+      req.body.transactionId = payment.source.id;
+      const newbooking = new Booking(req.body);
+      await newbooking.save();
+      const car = await Car.findOne({ _id: req.body.car });
+      console.log(req.body.car);
+      car.bookedTimeSlots.push(req.body.bookedTimeSlots);
+
+      await car.save();
+      res.send("Your booking is successfull");
+    } else {
+      return res.status(400).json(error);
+    }
   } catch (error) {
+    console.log(error);
     return res.status(400).json(error);
   }
 });
