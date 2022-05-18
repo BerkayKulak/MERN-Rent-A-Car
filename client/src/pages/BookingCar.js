7aaeb08d53bddbf60b3a21fc81848cfa6eea62ec
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-18 12:55:51 +0300

    Booking screen

--- client/src/pages/BookingCar.js
+++ client/src/pages/BookingCar.js
@@ -1,10 +1,45 @@
-import React from "react";
+import { Col, Row, Divider } from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import DefaultLayout from "../components/DefaultLayout";
+import Spinner from "../components/Spinner";
+import { getAllCars } from "../redux/actions/carsActions";
 
-function BookingCar() {
+function BookingCar({ match }) {
+  const { cars } = useSelector((state) => state.carsReducer);
+  const { loading } = useSelector((state) => state.alertsReducer);
+  const [car, setCar] = useState({});
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (cars.length === 0) {
+      dispatch(getAllCars());
+    } else {
+      setCar(cars.find((car) => car._id === match.params.carid));
+    }
+  }, [cars]);
   return (
     <DefaultLayout>
-      <h1>Booking Car Page</h1>
+      {loading === true && <Spinner />}
+      <Row
+        justify="center"
+        className="d-flex align-items-center"
+        style={{ minHeight: "90vh" }}
+      >
+        <Col lg={10} sm={24} xs={24}>
+          <img src={car.image} className="carimg2 bs1" />
+        </Col>
+        <Col lg={10} sm={24} xs={24} className="text-right">
+          <Divider type="horizontal" dashed>
+            Car Info
+          </Divider>
+          <div style={{ textAlign: "right" }}>
+            <p>{car.name}</p>
+            <p>{car.rentPerHour} Rent Per Hour</p>
+            <p>Fuel: {car.fuelType}</p>
+            <p>Max Persons: {car.capacity}</p>
+          </div>
+        </Col>
+      </Row>
     </DefaultLayout>
   );
 }
