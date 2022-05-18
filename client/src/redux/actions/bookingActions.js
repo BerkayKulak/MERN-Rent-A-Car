31a746eaf60b0c02a56a8e4549c7b5ccd2811db0
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-18 15:47:09 +0300

    User Bookings Logic

--- client/src/redux/actions/bookingActions.js
+++ client/src/redux/actions/bookingActions.js
@@ -8,24 +8,28 @@ export const bookCar = (reqObj) => async (dispatch) => {
     await axios.post("http://localhost:5000/api/bookings/bookcar", reqObj);
 
     dispatch({ type: "LOADING", payload: false });
-    message.success("Booking Successful");
+    message.success("Your car booked successfully");
+    setTimeout(() => {
+      window.location.href = "/userbookings";
+    }, 500);
   } catch (error) {
+    console.log(error);
     dispatch({ type: "LOADING", payload: false });
-    message.error("Booking Failed");
+    message.error("Something went wrong , please try later");
   }
 };
 
-export const getAllBookings=()=>async dispatch=>{
+export const getAllBookings = () => async (dispatch) => {
+  dispatch({ type: "LOADING", payload: true });
 
-    dispatch({type: 'LOADING' , payload:true})
-  
-    try {
-        const response = await axios.get('http://localhost:5000/api/bookings/getallbookings')
-        dispatch({type: 'GET_ALL_BOOKINGS', payload:response.data})
-        dispatch({type: 'LOADING' , payload:false})
-    } catch (error) {
-        console.log(error)
-        dispatch({type: 'LOADING' , payload:false})
-    }
-  
+  try {
+    const response = await axios.get(
+      "http://localhost:5000/api/bookings/getallbookings"
+    );
+    dispatch({ type: "GET_ALL_BOOKINGS", payload: response.data });
+    dispatch({ type: "LOADING", payload: false });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: "LOADING", payload: false });
   }
+};
